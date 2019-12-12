0df9cb74ef1edc679247acc1cb8a178ef8a74d3a
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-12 17:00:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  # yourbrothers = ["Tim", "Tom", "Jim"]
-brothers.each{|brother| puts "Stop hitting yourself #{brother}!"} code here
+  numbers = [1, 2, 3]
+.each{|brother| puts "Stop hitting yourself #{brother}!"} code here
 end
 
 brothers = ["Tim", "Tom", "Jim"]
