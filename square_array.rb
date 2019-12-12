fae5ee36c06f00ec02b9b17cc6fb6a3212a10e85
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-12 17:00:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
 def square_array(array)
-  # your code here
+  # yourbrothers = ["Tim", "Tom", "Jim"]
+brothers.each{|brother| puts "Stop hitting yourself #{brother}!"} code here
 end
+
+brothers = ["Tim", "Tom", "Jim"]
+brothers.each{|brother| puts "Stop hitting yourself #{brother}!"}
